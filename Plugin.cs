accef0443d5b844d1c993412635ff307c81d7889
Feen <finn>
2023-12-29 19:35:53 -0500

    Loads into steamVR

---
+++ Plugin.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Unity.Mono;
+using HarmonyLib;
+using UnityEngine.XR.Management;
+using UnityEngine;
+using VSVRMod2;
+using UnityEngine.XR.OpenXR;
+using UnityEngine.XR;
+
+namespace VSVR2;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+        InitializeXRRuntime();
+        StartDisplay();
+
+        Logger.LogInfo("Reached end of Plugin.Awake()");
+    }
+
+
+    private void InitializeXRRuntime()
+    {
+        // Set up the OpenXR loader
+        var generalSettings = ScriptableObject.CreateInstance<XRGeneralSettings>();
+        var managerSettings = ScriptableObject.CreateInstance<XRManagerSettings>();
+        var xrLoader = ScriptableObject.CreateInstance<OpenXRLoader>();
+
+        generalSettings.Manager = managerSettings;
+
+        ((List<XRLoader>)managerSettings.activeLoaders).Clear();
+        ((List<XRLoader>)managerSettings.activeLoaders).Add(xrLoader);
+
+        OpenXRSettings.Instance.renderMode = OpenXRSettings.RenderMode.MultiPass;
+        OpenXRSettings.Instance.depthSubmissionMode = OpenXRSettings.DepthSubmissionMode.Depth24Bit;
+
+        typeof(XRGeneralSettings).GetMethod("InitXRSDK", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(generalSettings, []);
+        typeof(XRGeneralSettings).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(generalSettings, []);
+
+        Logger.LogInfo("Initialized OpenXR Runtime");
+    }
+
+    private bool StartDisplay()
+    {
+        var displays = new List<XRDisplaySubsystem>();
+
+        SubsystemManager.GetInstances(displays);
+
+        if (displays.Count < 1)
+        {
+            return false;
+        }
+
+        displays[0].Start();
+
+        Logger.LogInfo("Started XR Display subsystem, welcome to VR!");
+
+        return true;
+    }
+}
