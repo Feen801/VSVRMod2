329e4d81762a294812f05d799fcea1b6c2009829
Feen <feen@feen.feen>
2024-07-06 01:55:39 -0400

    Corrected version numbers

--- Helper/Constants.cs
+++ Helper/Constants.cs
@@ -5,5 +5,5 @@ class Constants
     public const string MenuScene = "MainMenuScene";
     public const string UserPresence = "UserPresence";
     public const string VersionStringPrefix = "VSVR Mod";
-    public const string CurrentVersionString = "0.46R5-r1.6";
+    public const string CurrentVersionString = "0.46R5-r1.6.1";
 }
