d3f10d65427e93c2725c38d115787ed087291aaf
Feen <feen@feen.feen>
2025-01-03 00:28:41 -0500

    Removed VS version from the VR mod debug string in the overlay so I don't have to remember to change it anymore

--- Helper/Constants.cs
+++ Helper/Constants.cs
@@ -6,5 +6,5 @@ class Constants
     public const string MenuScene = "MainMenuScene";
     public const string UserPresence = "UserPresence";
     public const string VersionStringPrefix = "VSVR Mod";
-    public const string CurrentVersionString = "0.47R1-r" + MyPluginInfo.PLUGIN_VERSION;
+    public const string CurrentVersionString = MyPluginInfo.PLUGIN_VERSION;
 }
